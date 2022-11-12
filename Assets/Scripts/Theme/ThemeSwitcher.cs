12ec991eb503088bc5f66eb258bc87a06bfaa365
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2022-11-12 17:36:13 +0100

    Aggiungo animazione di transizione

--- Assets/Scripts/Theme/ThemeSwitcher.cs
+++ Assets/Scripts/Theme/ThemeSwitcher.cs
@@ -5,25 +5,42 @@ using System.Linq;
 
 public class ThemeSwitcher : MonoBehaviour
 {
+    private PlayerMovement player;
     public bool isDesert;
     public ThemeHandler[] handlers;
+    private GameObject[] transitionObjects;
+    public TransitionManager transitionManager;
+    public float speed;
+
     void Start()
     {
+        player = GetComponent<PlayerMovement>();
         // Utilizzo sto schifo per prendere tutti gli oggetti che implementano l'interfaccia ThemeHandler
         var mObjs = GameObject.FindObjectsOfType<MonoBehaviour>();
         handlers = (from a in mObjs where a.GetType().GetInterfaces().Any(k => k == typeof(ThemeHandler)) select (ThemeHandler)a).ToArray();
     }
 
     public void fireChange() {
-        if(isDesert) {
-            foreach(var handler in handlers) {
-                handler.onDesert();
+        // Transition
+        player.canMove = false;
+
+        transitionManager.onTransition(speed);
+
+        Utils.setTimeout(() => {
+            if(isDesert) {
+                foreach(var handler in handlers) {
+                    handler.onDesert();
+                }
+            } else {
+                foreach(var handler in handlers) {
+                    handler.onOcean();
+                }
             }
-        } else {
-            foreach(var handler in handlers) {
-                handler.onOcean();
-            }
-        }
+        }, 1f/3f / speed);
+
+        Utils.setTimeout(() => {
+            player.canMove = true;
+        }, 2f/3f / speed);
     }
 
     void Update()
---
+++ Assets/Scripts/Theme/TransitionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionManager : MonoBehaviour
+{
+
+    public Animator[] animators;
+
+    public void Start() {
+        animators = GetComponentsInChildren<Animator>();
+    }
+
+    public void onTransition(float speed) {
+        foreach(var animator in animators) {
+            animator.SetFloat("speed", speed);
+            animator.SetTrigger("transition");
+            animator.enabled = true;
+        }
+    }
+}
