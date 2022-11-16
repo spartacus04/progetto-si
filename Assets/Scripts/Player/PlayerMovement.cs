88fcc6ed5be6fb5af45b51077f85fe72924d5acc
zDe4N <103891170+zDe4N@users.noreply.github.com>
2022-11-16 14:35:42 +0100

    bomb spawnpoint

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,8 +13,17 @@ public class PlayerMovement : MonoBehaviour
     [HideInInspector]
     public Rigidbody2D rb;
     private static PlayerMovement instance;
+	Vector2 lastPosition;
+    [SerializeField]
+	GameObject instanziaBomba;
+	bool facingRight;
+	bool facingLeft;
+	bool facingDown;
+	bool facingUp;
+	[SerializeField]
+	float distanceBomb;
 
-    public void Awake()
+	public void Awake()
     {
         if(instance == null)
             instance = this;
@@ -25,18 +34,52 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+		lastPosition = this.transform.position;
     }
 
-    void Update()
+	void direction(float distanceBomb)
+	{
+		
+
+        facingRight = (movement.x > 0) ? true : false;
+		facingLeft = (movement.x < 0) ? true : false;
+		facingDown = (movement.y < 0) ? true : false;
+	    facingUp = (movement.y > 0) ? true : false;
+
+		if (facingRight)
+		{
+			instanziaBomba.transform.position = new Vector2(transform.position.x + distanceBomb, transform.position.y);
+		}
+		else if(facingLeft)
+		{
+			instanziaBomba.transform.position = new Vector2(transform.position.x - distanceBomb, transform.position.y);
+
+		}else if (facingUp)
+		{
+			instanziaBomba.transform.position = new Vector2(transform.position.x, transform.position.y+distanceBomb);
+		}
+		else if (facingDown)
+		{
+			instanziaBomba.transform.position = new Vector2(transform.position.x, transform.position.y-distanceBomb);
+
+		}
+
+
+
+	}
+
+	void Update()
     {
+		
         if (!canMove)
         {
             movement = Vector2.zero;
             return;
         }
         
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-    }
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+		direction(distanceBomb);
+	}
 
     void FixedUpdate()
     {
