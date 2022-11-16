6ffe0734e9245e0bacc45172aaac13cae3eb5bca
zDe4N <103891170+zDe4N@users.noreply.github.com>
2022-11-16 15:18:44 +0100

    Aggiunta
    quantità bombe
    controllo collider bombe

--- Assets/Scripts/Player/BombAbility.cs
+++ Assets/Scripts/Player/BombAbility.cs
@@ -4,38 +4,46 @@ using UnityEngine;
 
 public class BombAbility : MonoBehaviour
 {
+
 	[SerializeField] GameObject bomb;
     [SerializeField] GameObject spawnBomb;
 	public bool CanDropBomb;
 
 	public float bombTimer = 0.5f;
 	public float bombTimerReset = 0.5f;
-
 	public float bombRadius = 1f;
 
 	private float lastTimer;
 
+	[Header("Bomb Quantity")]
+	[SerializeField] int maxQuantity;
+	private int quantityBomb=0;
 	private void Start()
 	{
 		lastTimer = Time.time;
 	}
 
+
 	
 	private void Update()
 	{
+		var controllo = Physics2D.OverlapCircleAll(spawnBomb.transform.position, 0.2f);
+		print(controllo.Length);
 		
-
-		if(lastTimer + bombTimer + bombTimerReset + Time.deltaTime < Time.time)
+		if (lastTimer + bombTimer + bombTimerReset + Time.deltaTime < Time.time)
 		{
 			CanDropBomb = true;
 		}
+		
 
-		if(CanDropBomb && Input.GetKeyDown(KeyCode.Q))
+		if (CanDropBomb && Input.GetKeyDown(KeyCode.Q) && maxQuantity > quantityBomb && controllo.Length <=1)
 		{
+
 			CanDropBomb = false;
 			lastTimer = Time.time;
 
 			var bombI = Instantiate(bomb, spawnBomb.transform.position, Quaternion.identity);
+			quantityBomb++;
 			bombI.GetComponent<Bomb>().setup(bombRadius, bombTimer);
 		}
 	}
