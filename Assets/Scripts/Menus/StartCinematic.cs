8c7b959465d0cfdcdf83f4255911da95073fbaa3
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2022-11-28 12:55:08 +0100

    Aggiungo font personalizzato

--- Assets/Scripts/Menus/StartCinematic.cs
+++ Assets/Scripts/Menus/StartCinematic.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.Video;
 
 public class StartCinematic : MonoBehaviour
 {
 	private VideoPlayer player;
+	public float timeout;
+	public TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +25,48 @@ public class StartCinematic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKey) {
-			Destroy(gameObject);
+        if(Input.anyKeyDown) {
+			if(text.color.a >= 0.3f)	 {
+				Destroy(gameObject);
+				return;
+			}
+
+			StopAllCoroutines();
+			StartCoroutine(fadeText());
+			StartCoroutine(animateText());
 		}
     }
+
+	IEnumerator fadeText() {
+		text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+		// fade text gradually to 0 in timeout seconds
+
+		for (float t = 0.0f; t < timeout; t += Time.deltaTime) {
+			float normalizedTime = t / timeout;
+			text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(1, 0, normalizedTime));
+			yield return null;
+		}
+
+		text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+	}
+
+	IEnumerator animateText() {
+		text.text = "Skip";
+
+		yield return new WaitForSeconds(0.33f);
+
+		text.text = "Skip.";
+
+		yield return new WaitForSeconds(0.33f);
+
+		text.text = "Skip..";
+
+		yield return new WaitForSeconds(0.33f);
+
+		text.text = "Skip...";
+
+		yield return new WaitForSeconds(0.33f);
+
+		StartCoroutine(animateText());
+	}
 }
